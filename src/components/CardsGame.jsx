6ec1a6f88a7fe06273c14046b8252d47e102eb44
kenny <kenny909@mat.umk.pl>
2024-07-22 22:09:16 +0200

    Finish page look

--- src/components/CardsGame.jsx
+++ src/components/CardsGame.jsx
@@ -4,16 +4,18 @@ import '../styles/CardsGame.css'
 
 export function CardsGame({ cardsList }) {
     return (
-        <>
-            <h1>Memory Game</h1>
-            <p>Get points by clicking on an image but don&apos;t click on any more than once</p>
+        <div className="cards-game">
+            <header>
+                <h1>Memory Game</h1>
+                <p>Get points by clicking on an image but don&apos;t click on any more than once</p>
+            </header>
             <div className="cards-container">
                 <ul className="cards-list">
                     {cardsList.map(card => <Card key={card.id} item={card} />)}
                 </ul>
 
             </div>
-        </>
+        </div>
     );
 
 }
